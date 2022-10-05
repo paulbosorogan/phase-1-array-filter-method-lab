1278a3af4c749135977626a6340fa846dd7ba16f
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-10-05 17:38:57 -0400

    commit tree d8da40c0d8113ef76d846297f22805a684bab422

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    const newDrivers = [];
+    for(const user of drivers){
+        if(user.toLowerCase() === string.toLowerCase())
+        newDrivers.push(user)
+    }
+    return newDrivers
+}
+
+function fuzzyMatch(driverArray, string){
+    return driverArray.filter((element) =>
+        element.indexOf(string) === 0)
+    }
+
+
+
+function matchName(driverObj, string){
+   return driverObj.filter((element)=>
+   element.name === string)
+}
